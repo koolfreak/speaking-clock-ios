4f7d0cb53fc62f7524bbc2e58e4422063e63297d
Eman Nollase <eman.nollase@gmail.com>
2023-09-20 21:12:26 +0800

    Initial Commit

---
+++ SpeakingClock/SpeakingClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakingClockApp.swift
+//  SpeakingClock
+//
+//  Created by Eman on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeakingClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
